bbad984b5462713b76085d44b7ef774dfa178006
ai-trip-planner <pathanfaizan680@gmail.com>
2026-07-23 19:16:57 +0530

    comment out JPA and PostgreSQL dependencies

--- ai-trip-planner-backend/src/main/java/edu/ai_trip_planner/controller/AIController.java
+++ ai-trip-planner-backend/src/main/java/edu/ai_trip_planner/controller/AIController.java
@@ -1,17 +1,18 @@
 package edu.ai_trip_planner.controller;
 
 import edu.ai_trip_planner.Dto.TripPlanResponse;
-import edu.ai_trip_planner.Entity.TripEntity;
+// import edu.ai_trip_planner.Entity.TripEntity;
 import edu.ai_trip_planner.aiServices.Aiservice;
-import edu.ai_trip_planner.repository.aiRepository;
+// import edu.ai_trip_planner.repository.aiRepository;
 import edu.ai_trip_planner.Dto.RequestPlan;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
+// import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 @CrossOrigin("*")
 @RequestMapping("/api/ai")
 @RestController
@@ -21,19 +22,18 @@ public class AIController {
 
     private final Aiservice as;
 
-    private final aiRepository repo;
+    // private final aiRepository repo;
 
-@PostMapping ("/plan")
-    public TripPlanResponse getPlane(@Valid  @RequestBody RequestPlan plan){
+    @PostMapping("/plan")
+    public TripPlanResponse getPlane(@Valid @RequestBody RequestPlan plan) {
 
-    return as.getPlan(plan);
-}
+        return as.getPlan(plan);
+    }
 
-@GetMapping("/history")
-public List<TripEntity> getHistory(){
+    // @GetMapping("/history")
+    // public List<TripEntity> getHistory(){
 
-    return repo.findAll();
-}
-  
+    // return repo.findAll();
+    // }
 
 }
